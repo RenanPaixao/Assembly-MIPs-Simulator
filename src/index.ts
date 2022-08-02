f6515a5ef0374b678bb1064a84394b01a960e18a
renan <renanlemos21@gmail.com>
2022-08-02 07:48:57 -0300

    feat: init all registers

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/** Create all numeric registers and no numeric also as entries to create an object to manipulate them.
+ * These registers are with 0 as value.
+ */
+const numericRegisters = Array(32).fill(0).map((_, i) => [`$${i}`, 0])
+const nonNumericRegisters = [['pc', 0], ['hi', 0], ['lo', 0]]
+
+const registers: Record<string, any> = Object.fromEntries([...numericRegisters, ...nonNumericRegisters])
+
+/** Add values different from 0 to the registers that need it. **/
+registers.$28 = 268468224
+registers.$29 = 2147479548
+registers.lo = 4194304
+
+/** -------------------------------------------------- **/
+
+
+
+
+
